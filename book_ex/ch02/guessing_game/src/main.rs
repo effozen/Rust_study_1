6daf85d5485d59389f99938646128cb9e694b7b9
OAO <ashgrayblue0@gmail.com>
2023-05-03 12:48:20 +0900

    practice the guessing game

--- book_ex/ch02/guessing_game/src/main.rs
+++ book_ex/ch02/guessing_game/src/main.rs
@@ -3,9 +3,11 @@ use std::io;
 fn main() {
 	println!("Guess the number");
 
-	let mut guess::String = String::new();
+	let mut guess = String::new();
 
 	io::stdin()
 		.read_line(&mut guess)
-		.expect("Input Error!");
+		.expect("Failed to read line");
+
+	println!("You guessed: {guess}");
 }
