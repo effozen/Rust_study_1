4949374d8c58e98536dbf9d51a9fb790030daa07
OAO <ashgrayblue0@gmail.com>
2023-05-04 15:25:00 +0900

    if_expression practice

---
+++ book_ex/ch03/if_expression/target/debug/deps/if_expression-b7397867d7c95d8c.d
@@ -0,0 +1,5 @@
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/if_expression/target/debug/deps/if_expression-b7397867d7c95d8c: src/main.rs
+
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/if_expression/target/debug/deps/if_expression-b7397867d7c95d8c.d: src/main.rs
+
+src/main.rs:
---
+++ book_ex/ch03/if_expression/target/debug/if_expression.d
@@ -0,0 +1,1 @@
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/if_expression/target/debug/if_expression: /Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/if_expression/src/main.rs
