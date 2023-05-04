1b367c94425334c90406fee8571da659bc2c41ec
OAO <ashgrayblue0@gmail.com>
2023-05-04 14:23:21 +0900

    numeric operation practice

---
+++ book_ex/ch03/numeric_operations/target/debug/deps/numeric_operations-822fe9932d377db0.d
@@ -0,0 +1,5 @@
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/numeric_operations/target/debug/deps/numeric_operations-822fe9932d377db0: src/main.rs
+
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/numeric_operations/target/debug/deps/numeric_operations-822fe9932d377db0.d: src/main.rs
+
+src/main.rs:
---
+++ book_ex/ch03/numeric_operations/target/debug/numeric_operations.d
@@ -0,0 +1,1 @@
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/numeric_operations/target/debug/numeric_operations: /Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/numeric_operations/src/main.rs
