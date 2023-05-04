9bfbd249550802dc417313e7c0f417f9e9ccf46a
OAO <ashgrayblue0@gmail.com>
2023-05-04 13:54:19 +0900

    mutability and immutability practice

---
+++ book_ex/ch03/variables/target/debug/deps/variables-b20f90f52138261a.d
@@ -0,0 +1,5 @@
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/variables/target/debug/deps/variables-b20f90f52138261a: src/main.rs
+
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/variables/target/debug/deps/variables-b20f90f52138261a.d: src/main.rs
+
+src/main.rs:
