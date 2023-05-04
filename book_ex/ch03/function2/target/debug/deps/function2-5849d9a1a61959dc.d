394003d6ac79a4155a8fdd9e934ae96bcf98ed8a
OAO <ashgrayblue0@gmail.com>
2023-05-04 14:43:23 +0900

    function prac2

---
+++ book_ex/ch03/function2/target/debug/deps/function2-5849d9a1a61959dc.d
@@ -0,0 +1,5 @@
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/function2/target/debug/deps/function2-5849d9a1a61959dc: src/main.rs
+
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/function2/target/debug/deps/function2-5849d9a1a61959dc.d: src/main.rs
+
+src/main.rs:
---
+++ book_ex/ch03/function2/target/debug/function2.d
@@ -0,0 +1,1 @@
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/function2/target/debug/function2: /Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/function2/src/main.rs
