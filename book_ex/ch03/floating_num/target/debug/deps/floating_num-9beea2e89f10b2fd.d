8daf6e88b48701c016df4cfe53ad00cdeaca9af7
OAO <ashgrayblue0@gmail.com>
2023-05-04 14:17:40 +0900

    floating point number practice

---
+++ book_ex/ch03/floating_num/target/debug/deps/floating_num-9beea2e89f10b2fd.d
@@ -0,0 +1,5 @@
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/floating_num/target/debug/deps/floating_num-9beea2e89f10b2fd: src/main.rs
+
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/floating_num/target/debug/deps/floating_num-9beea2e89f10b2fd.d: src/main.rs
+
+src/main.rs:
---
+++ book_ex/ch03/floating_num/target/debug/floating_num.d
@@ -0,0 +1,1 @@
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/floating_num/target/debug/floating_num: /Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/floating_num/src/main.rs
