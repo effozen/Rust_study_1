cc37fdd8ffc7afbb5e09c2a968e158b0b7729ba2
OAO <ashgrayblue0@gmail.com>
2023-05-04 14:07:01 +0900

    shadowing practice

---
+++ book_ex/ch03/shadowing/target/debug/deps/shadowing-412e351210c8edfe.d
@@ -0,0 +1,5 @@
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/shadowing/target/debug/deps/shadowing-412e351210c8edfe: src/main.rs
+
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/shadowing/target/debug/deps/shadowing-412e351210c8edfe.d: src/main.rs
+
+src/main.rs:
---
+++ book_ex/ch03/shadowing/target/debug/shadowing.d
@@ -0,0 +1,1 @@
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/shadowing/target/debug/shadowing: /Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/shadowing/src/main.rs
