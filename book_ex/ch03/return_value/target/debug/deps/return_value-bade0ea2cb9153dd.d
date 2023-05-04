221c55eabafbca120f068a360b22142b7f515a27
OAO <ashgrayblue0@gmail.com>
2023-05-04 14:55:34 +0900

    return value prac

---
+++ book_ex/ch03/return_value/target/debug/deps/return_value-bade0ea2cb9153dd.d
@@ -0,0 +1,5 @@
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/return_value/target/debug/deps/return_value-bade0ea2cb9153dd: src/main.rs
+
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/return_value/target/debug/deps/return_value-bade0ea2cb9153dd.d: src/main.rs
+
+src/main.rs:
---
+++ book_ex/ch03/return_value/target/debug/return_value.d
@@ -0,0 +1,1 @@
+/Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/return_value/target/debug/return_value: /Users/oao/data/jd/dev/01_Study/02_rust/rust_docs/book_ex/ch03/return_value/src/main.rs
