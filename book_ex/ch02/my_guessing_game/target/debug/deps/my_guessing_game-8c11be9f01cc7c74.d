43ec62d85ae3267aafc67c54e68d21e0c2206168
OAO <ashgrayblue0@gmail.com>
2023-07-18 16:45:44 +0900

    ch02::my_guessing_game

---
+++ book_ex/ch02/my_guessing_game/target/debug/deps/my_guessing_game-8c11be9f01cc7c74.d
@@ -0,0 +1,5 @@
+/home/oao/myd/rust/book_ex/ch02/my_guessing_game/target/debug/deps/my_guessing_game-8c11be9f01cc7c74: src/main.rs
+
+/home/oao/myd/rust/book_ex/ch02/my_guessing_game/target/debug/deps/my_guessing_game-8c11be9f01cc7c74.d: src/main.rs
+
+src/main.rs:
---
+++ book_ex/ch02/my_guessing_game/target/debug/my_guessing_game.d
@@ -0,0 +1,1 @@
+/home/oao/myd/rust/book_ex/ch02/my_guessing_game/target/debug/my_guessing_game: /home/oao/myd/rust/book_ex/ch02/my_guessing_game/src/main.rs
