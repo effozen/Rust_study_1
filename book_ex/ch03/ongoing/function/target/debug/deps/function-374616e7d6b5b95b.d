449d309e593623edf89a845019173d48cec15c48
OAO <ashgrayblue0@gmail.com>
2023-07-19 16:52:01 +0900

    ch03-3::function

---
+++ book_ex/ch03/ongoing/function/target/debug/deps/function-374616e7d6b5b95b.d
@@ -0,0 +1,5 @@
+/home/oao/myd/rust/book_ex/ch03/ongoing/function/target/debug/deps/function-374616e7d6b5b95b: src/main.rs
+
+/home/oao/myd/rust/book_ex/ch03/ongoing/function/target/debug/deps/function-374616e7d6b5b95b.d: src/main.rs
+
+src/main.rs:
---
+++ book_ex/ch03/ongoing/function/target/debug/function.d
@@ -0,0 +1,1 @@
+/home/oao/myd/rust/book_ex/ch03/ongoing/function/target/debug/function: /home/oao/myd/rust/book_ex/ch03/ongoing/function/src/main.rs
