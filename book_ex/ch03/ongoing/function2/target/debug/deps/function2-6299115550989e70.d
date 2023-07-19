cd989180ff417f2fee8789c19a3ec479e3c1f094
OAO <ashgrayblue0@gmail.com>
2023-07-19 21:25:45 +0900

    ch03::update

---
+++ book_ex/ch03/ongoing/function2/target/debug/deps/function2-6299115550989e70.d
@@ -0,0 +1,5 @@
+/home/oao/myd/rust/book_ex/ch03/ongoing/function2/target/debug/deps/function2-6299115550989e70: src/main.rs
+
+/home/oao/myd/rust/book_ex/ch03/ongoing/function2/target/debug/deps/function2-6299115550989e70.d: src/main.rs
+
+src/main.rs:
